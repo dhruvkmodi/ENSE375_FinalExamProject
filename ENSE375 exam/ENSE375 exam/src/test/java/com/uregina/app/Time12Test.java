1381e81d1e727534319412e317a42d051b590189
Dhruv Modi <dhruvkmodi@gmail.com>
2021-04-27 16:30:16 -0600

    Added/Updated

--- ENSE375 exam/ENSE375 exam/src/test/java/com/uregina/app/Time12Test.java
+++ ENSE375 exam/ENSE375 exam/src/test/java/com/uregina/app/Time12Test.java
@@ -1,11 +1,40 @@
 package com.uregina.app;
 import static org.junit.Assert.assertTrue;
+
+import com.uregina.exceptions.InvalidTimeException;
+
 import org.junit.Test;
 
 public class Time12Test 
 {
    @Test
-   public void Subtract_Time12_True()
+   public void Subtract_Time12PostiveDifference_True() throws InvalidTimeException
+   {
+   Time12 timetest1 = new Time12(2, 30, AmPm.am);
+   Time12 timetest2 = new Time12(2, 30, AmPm.am);
+   int True = 0;  
+   True = Time12.subtract(timetest1, timetest2);
+   if (True > 0)
+   {
+   assertTrue(true);
+   }
+   } 
+
+   @Test
+   public void Subtract_Time12NegativeDifference_True() throws InvalidTimeException
+   {
+   Time12 timetest1 = new Time12(2, 30, AmPm.am);
+   Time12 timetest2 = new Time12(2, 30, AmPm.am);
+   int True = 0;  
+   True = Time12.subtract(timetest1, timetest2);
+   if (True > 0)
+   {
+   assertTrue(true);
+   }
+   } 
+
+   @Test
+   public void Subtract_Time12ZeroDifference_True()
    {
     assertTrue(true);
    } 
